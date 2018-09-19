ac809b58543d09b08219c8bf80c78ff2426cd4c7
Gott50 <xxGott50xx@gmail.com>
2018-09-19 13:38:19 +0200

    add StartMessage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import {Widget} from 'react-chat-widget';
+import {Widget, addResponseMessage} from 'react-chat-widget';
 
 import 'react-chat-widget/lib/styles.css';
 
 class App extends Component {
+    componentDidMount() {
+        addResponseMessage("Welcome to this awesome chat!");
+    }
+
     render() {
         return (
             <div className="App">
