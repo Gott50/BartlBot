8ed83e9bcf0b4e8c90086b8477d614d982e021c0
Gott50 <xxGott50xx@gmail.com>
2018-09-19 13:09:20 +0200

    import react-chat-widget

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Widget} from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Widget/>
+            </div>
+        );
+    }
 }
 
 export default App;
